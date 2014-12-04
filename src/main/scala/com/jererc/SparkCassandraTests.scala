66b978a22094a730d3441d70511ee1d9825a7170
Jerome Clerc <jererc@gmail.com>
2014-12-04 11:09:34 +0000

    add pair RDD tests

--- src/main/scala/com/jererc/SparkCassandraTests.scala
+++ src/main/scala/com/jererc/SparkCassandraTests.scala
@@ -11,7 +11,12 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql._
 import org.apache.spark.sql.cassandra.CassandraSQLContext
 
+// PairRDDFunctions
+import org.apache.spark.SparkContext._
+
 import net.liftweb.json._
+
+case class TestData(id: Int, data: String)
 
 object SparkCassandraTests {
 
@@ -72,7 +77,9 @@ object SparkCassandraTests {
 
     var errors = List[String]()
 
-    // Test input table
+    //
+    // Test table read and write
+    //
     val rdd = sc.cassandraTable(conf.cassandraKeyspace, conf.cassandraInputTable)
     val count = 10000
     val data = (0 to count - 1) map (n => (n, s"value $n"))
@@ -112,10 +119,12 @@ object SparkCassandraTests {
     val result4 = rdd7.map(row => (row.getInt("id"), row.getString("data"))).collect
     val expected4 = Array((11, "value 11"))
     if (result4.deep != expected4.deep) {
-      errors :+= "invalid where content"
+      errors :+= "invalid where result"
     }
 
+    //
     // Test SQL
+    //
     val cc = new CassandraSQLContext(sc)
     val rdd8: SchemaRDD = cc.sql(s"SELECT * from ${conf.cassandraKeyspace}.${conf.cassandraInputTable} WHERE id < 10")
     val result5 = rdd8.map(e => (e.getInt(0), e.getString(1))).collect.sortBy(_._1)
@@ -124,7 +133,9 @@ object SparkCassandraTests {
       errors :+= "invalid sql result"
     }
 
+    //
     // Test common transformations and actions
+    //
     val rdd9 = sc.cassandraTable(conf.cassandraKeyspace, conf.cassandraInputTable)
     val rdd10 = rdd9
       .filter(row => (row.getInt("id") >= 100 && row.getInt("id") < 110) || (row.getInt("id") >= 1000 && row.getInt("id") < 1010))
@@ -137,7 +148,6 @@ object SparkCassandraTests {
     if (result6.deep != expected6.deep) {
       errors :+= "invalid map result"
     }
-
     val rdd11 = rdd9
       .filter(row => row.getInt("id") >= 100 && row.getInt("id") < 110)
       .map(row => row.getInt("id"))
@@ -145,6 +155,64 @@ object SparkCassandraTests {
     val expected7 = (100 to 109) reduce ((x, y) => x + y)
     if (result7 != expected7) {
       errors :+= "invalid fold result"
+    }
+
+    // Test custom type
+    val rdd12 = sc.cassandraTable[TestData](conf.cassandraKeyspace, conf.cassandraInputTable)
+    val result8 = rdd12.filter(r => r.id < 10).collect.sortBy(_.id)
+    val expected8 = (0 to 9).toArray.map(n => TestData(n, s"value $n"))
+    if (result8.deep != expected8.deep) {
+      errors :+= "invalid custom type result"
+    }
+
+    //
+    // Test pair RDD
+    //
+    connector.withSessionDo { session =>
+      session.execute(s"DROP TABLE IF EXISTS ${conf.cassandraKeyspace}.${conf.cassandraOutputTable};")
+      session.execute(s"CREATE TABLE ${conf.cassandraKeyspace}.${conf.cassandraOutputTable} (id int PRIMARY KEY, data text);")
+    }
+    val data2 = Seq((1, "v1"), (2, "v2"), (3, "v1"), (4, "v2"), (5, "v3"))
+    val collection2 = sc.parallelize(data2)
+    collection2.saveToCassandra(conf.cassandraKeyspace, conf.cassandraOutputTable,
+      SomeColumns("id", "data"))
+    val rdd13 = sc.cassandraTable[TestData](conf.cassandraKeyspace, conf.cassandraOutputTable)
+      .map(r => (r.data, r.id))
+
+    // Test groupByKey
+    val rdd14 = rdd13.groupByKey()
+    val result9 = rdd14.sortBy(_._1).collect
+    val expected9 = Array(("v1", Seq(1, 3)), ("v2", Seq(2, 4)), ("v3", Seq(5)))
+    if (result9.deep != expected9.deep) {
+      errors :+= "invalid groupByKey result"
+    }
+    val result10 = rdd14.keys.collect.sorted
+    val expected10 = Array("v1", "v2", "v3")
+    if (result10.deep != expected10.deep) {
+      errors :+= "invalid groupByKey keys"
+    }
+    val result11 = rdd14.values.collect.sorted
+    val expected11 = Array(Seq(1, 3), Seq(2, 4), Seq(5))
+    if (result11.deep != expected11.deep) {
+      errors :+= "invalid groupByKey values"
+    }
+
+    // Test reduceByKey
+    val rdd15 = rdd13.reduceByKey((x, y) => x + y)
+    val result12 = rdd15.sortBy(_._1).collect
+    val expected12 = Array(("v1", 4), ("v2", 6), ("v3", 5))
+    if (result12.deep != expected12.deep) {
+      errors :+= "invalid reduceByKey result"
+    }
+    val result13 = rdd15.keys.collect.sorted
+    val expected13 = Array("v1", "v2", "v3")
+    if (result13.deep != expected13.deep) {
+      errors :+= "invalid reduceByKey keys"
+    }
+    val result14 = rdd15.values.collect.sorted
+    val expected14 = Array(4, 5, 6)
+    if (result14.deep != expected14.deep) {
+      errors :+= "invalid reduceByKey values"
     }
 
     sc.stop()
